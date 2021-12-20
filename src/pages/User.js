1b8889f35fe230a5e4537dda4c7cd974ce363730
Chloé R <clositi@live.fr>
2021-12-20 12:39:27 +0100

    added SideNav to User page

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import SideNav from "../components/SideNav";
 
 function User() {
 	return (
-<Header/>
+		<React.Fragment>
+			<Header />
+			<SideNav />
+		</React.Fragment>
 	);
 }
 
