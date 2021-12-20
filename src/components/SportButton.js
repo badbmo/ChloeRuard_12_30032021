28395f68558cb2321d3a32155e7ea798db32e91b
Chloé R <clositi@live.fr>
2021-12-20 12:38:42 +0100

    created SportButton component

---
+++ src/components/SportButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/sportButton.css";
+
+function SportButton({src, alt}) {
+	return (
+		<button className="sportButton">
+			<img src={src} alt={alt} className="sportButton__icon" />
+		</button>
+	);
+}
+
+export default SportButton;
