bda0c4ef23f6626b614be2b657849f7ed0cd02b2
Chloé R <clositi@live.fr>
2021-12-20 16:59:27 +0100

    created ChartKpi component

---
+++ src/components/ChartKpi.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/chartKpi.css";
+
+function ChartKpi() {
+	return (
+		<article className="chartKpi">
+			<div className="chartKpi__graph">
+			</div>
+		</article>
+	);
+}
+
+export default ChartKpi;
