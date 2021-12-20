6ef29594608a846b5b1cb0303d37df2078dde3f2
Chloé R <clositi@live.fr>
2021-12-20 16:59:54 +0100

    created ChartWeight component

---
+++ src/components/ChartWeight.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/chartWeight.css";
+
+function ChartWeight() {
+	return (
+		<article className="chartWeight">
+			<div className="chartWeight__graph">
+			</div>
+		</article>
+	);
+}
+
+export default ChartWeight;
