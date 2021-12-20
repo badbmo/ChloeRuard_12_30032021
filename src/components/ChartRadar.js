7c81a4de8473575075cf3a0b099c529c65554ba5
Chloé R <clositi@live.fr>
2021-12-20 16:59:40 +0100

    created ChartRadar component

---
+++ src/components/ChartRadar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/chartRadar.css";
+
+function ChartRadar() {
+	return (
+		<article className="chartRadar">
+			<div className="chartRadar__graph">
+			</div>
+		</article>
+	);
+}
+
+export default ChartRadar;
