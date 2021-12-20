6a8e7ff6f1eda832d2151467ec38f3e623c05c65
Chloé R <clositi@live.fr>
2021-12-20 17:00:28 +0100

    added Charts to Dashboard improved css of user page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,12 +6,22 @@ import Fire from "../assets/icon_fire.svg";
 import Chicken from "../assets/icon_chicken.svg";
 import Apple from "../assets/icon_apple.svg";
 import Hamburger from "../assets/icon_cheeseburger.svg";
+import ChartGoal from "./ChartGoal";
+import ChartKpi from "./ChartKpi";
+import ChartRadar from "./ChartRadar";
+import ChartWeight from "./ChartWeight";
 
 function Dashboard() {
 	return (
 		<section className="dashboard">
 			<Headline name="Thomas" />
 			<section className="dashboard__data">
+				<section className="dashboard__charts">
+					<ChartWeight />
+					<ChartGoal />
+					<ChartRadar />
+					<ChartKpi />
+				</section>
 				<section className="dashboard__macroList">
 					<MacroWidget src={Fire} amount="1930kCal" type="Calories" color="red" />
 					<MacroWidget src={Chicken} amount="155g" type="Protéines" color="blue" />
