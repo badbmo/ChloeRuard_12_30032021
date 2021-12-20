38ed1a955bb6928a50bc3c9e0435231c61dd216e
Chloé R <clositi@live.fr>
2021-12-20 01:39:54 +0100

    created User page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import User from "./components/User";
+import User from "./pages/User";
 
 function App() {
 	return (
