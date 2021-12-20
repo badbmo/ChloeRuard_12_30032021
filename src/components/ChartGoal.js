201c96039d68a437e15be9573d8ade300bc05444
Chloé R <clositi@live.fr>
2021-12-20 16:59:13 +0100

    created ChartGoal component

---
+++ src/components/ChartGoal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/chartGoal.css";
+
+function ChartGoal() {
+	return (
+		<article className="chartGoal">
+			<div className="chartGoal__graph">
+			</div>
+		</article>
+	);
+}
+
+export default ChartGoal;
