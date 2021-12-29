c896fc4eea5a4ea4da44385a2fbd694de5c8a4a7
Chloé R <clositi@live.fr>
2021-12-29 19:27:11 +0100

    created ChartWeight with recharts

--- src/components/ChartWeight.js
+++ src/components/ChartWeight.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "../style/chartWeight.css";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
-function ChartWeight() {
+function ChartWeight({ activity }) {
+	const data = activity;
+
 	return (
 		<article className="chartWeight">
-			<div className="chartWeight__graph">
-			</div>
+			<ResponsiveContainer width="100%" height="100%">
+				<BarChart
+					width={500}
+					height={300}
+					data={data}
+					margin={{
+						top: 5,
+						right: 30,
+						left: 20,
+						bottom: 5,
+					}}
+				>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="day" />
+					<YAxis />
+					<Tooltip />
+					<Legend />
+					<Bar dataKey="kilogram" fill="#8884d8" />
+					<Bar dataKey="calories" fill="#82ca9d" />
+				</BarChart>
+			</ResponsiveContainer>
 		</article>
 	);
 }
