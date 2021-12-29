e4e467082487852982e49c1993483d29b7a16918
Chloé R <clositi@live.fr>
2021-12-29 19:26:56 +0100

    created ChartRadar with recharts

--- src/components/ChartRadar.js
+++ src/components/ChartRadar.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "../style/chartRadar.css";
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from "recharts";
 
-function ChartRadar() {
+function ChartRadar({ performance }) {
+	const data = performance;
+
 	return (
 		<article className="chartRadar">
-			<div className="chartRadar__graph">
-			</div>
+			<ResponsiveContainer width="100%" height="100%">
+				<RadarChart cx="50%" cy="50%" outerRadius={150} data={data}>
+					<PolarGrid radialLines={false} stroke="white" />
+					<PolarAngleAxis dataKey="kind" tick={true} />
+					<Radar name="Performance" dataKey="value" stroke="#FF0000" fill="#FF0000" fillOpacity={0.6} />
+				</RadarChart>
+			</ResponsiveContainer>
 		</article>
 	);
 }
