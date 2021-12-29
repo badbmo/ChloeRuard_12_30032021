245fbc772acc579563818a0341d3b9068e785e3a
Chloé R <clositi@live.fr>
2021-12-29 19:26:31 +0100

    created ChartGoal graph with recharts

--- src/components/ChartGoal.js
+++ src/components/ChartGoal.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "../style/chartGoal.css";
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from "recharts";
 
-function ChartGoal() {
+function ChartGoal({ averageSessions }) {
+	const data = averageSessions;
+
 	return (
 		<article className="chartGoal">
-			<div className="chartGoal__graph">
-			</div>
+			<ResponsiveContainer width="100%" height="100%">
+				<LineChart
+					data={data}
+					margin={{
+						top: 5,
+						right: 30,
+						left: 20,
+						bottom: 5,
+					}}
+				>
+					<XAxis dataKey="day" />
+					<Tooltip />
+					<Line type="monotone" dataKey="sessionLength" stroke="#8884d8" activeDot={{ r: 8 }} />
+				</LineChart>
+			</ResponsiveContainer>
 		</article>
 	);
 }
