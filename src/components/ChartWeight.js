76e66a977d3ebf2248157069aafaf99a0c5b08d9
Chloé R <clositi@live.fr>
2021-12-30 00:26:34 +0100

    styled ChartWeight component

--- src/components/ChartWeight.js
+++ src/components/ChartWeight.js
@@ -1,31 +1,56 @@
 import React from "react";
 import "../style/chartWeight.css";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
 function ChartWeight({ activity }) {
+
 	const data = activity;
+
+	const CustomTooltip = ({ active, payload, label }) => {
+		if (active && payload && payload.length) {
+			return (
+				<div className="custom-tooltip">
+					<p>{`${payload[0].value}kg`}</p>
+					<p>{`${payload[1].value}Kcal`}</p>
+				</div>
+			);
+		}
+		return null;
+	};
+
+	const CustomXAxis = (tick) => {
+		return tick + 1;
+	};
 
 	return (
 		<article className="chartWeight">
-			<ResponsiveContainer width="100%" height="100%">
-				<BarChart
-					width={500}
-					height={300}
-					data={data}
-					margin={{
-						top: 5,
-						right: 30,
-						left: 20,
-						bottom: 5,
-					}}
+			<div className="chartWeight__info">
+				<span className="chartWeight__title">
+					Activité quotidienne
+				</span>
+				<div className="chartWeight__legend">
+					<svg height="15" width="15"><circle cx="50%" cy="50%" fill="#282D30" r="4"></circle></svg>
+					<span className="chartWeight__legend-weight">
+						Poids (kg)
+					</span>
+					<svg height="15" width="15"><circle cx="50%" cy="50%" fill="#FF0000" r="4"></circle></svg>
+					<span className="chartWeight__legend-calory">
+						Calories brûlées (kCal)
+						</span>
+				</div>
+			</div>
+			<ResponsiveContainer width="100%" height="78%">
+				<BarChart data={data}
+					margin={{ top: 5, right: 30, left: 30, bottom: 30, }}
+					barGap={8}
 				>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="day" />
-					<YAxis />
-					<Tooltip />
-					<Legend />
-					<Bar dataKey="kilogram" fill="#8884d8" />
-					<Bar dataKey="calories" fill="#82ca9d" />
+					<CartesianGrid strokeDasharray="3 3" vertical={false}  />
+					<XAxis tickFormatter={CustomXAxis} tickLine={false} tickMargin={15} tick={{ fill: "#9B9EAC" }} />
+					<YAxis dataKey="kilogram" yAxisId="kilogram" orientation="right" type="number" domain={['dataMin-7', 'dataMax+3']}  axisLine={false} tickLine={false} tick={{ fill: "#9B9EAC" }} tickMargin={30} />
+					<YAxis dataKey="calories" yAxisId="calories" orientation="left" type="number" domain={[0 , 'dataMax+20']} hide={true} />
+					<Tooltip content={<CustomTooltip />} cursor={{ stroke: '#C4C4C4', opacity: 0.5 }} offset={40}/>
+					<Bar dataKey="kilogram" yAxisId="kilogram" fill="#282D30" barSize={10} legendType="none" radius={[5, 5, 0, 0]}/>
+					<Bar dataKey="calories" yAxisId="calories" fill="#FF0000" barSize={10} legendType="none" radius={[5, 5, 0, 0]}/>
 				</BarChart>
 			</ResponsiveContainer>
 		</article>
