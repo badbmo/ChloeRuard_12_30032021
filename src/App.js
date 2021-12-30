daff0cd0fbbe6a59f6c56549144e28f1fc117fd4
Chloé R <clositi@live.fr>
2021-12-30 23:54:32 +0100

    gathered all style sheets in style directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./style/App.css";
 import User from "./pages/User";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
