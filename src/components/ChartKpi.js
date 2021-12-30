f640bc00d8d5df03229fda9965ebd60fbbb94216
Chloé R <clositi@live.fr>
2021-12-30 17:47:35 +0100

    finished style ChartKpi component

--- src/components/ChartKpi.js
+++ src/components/ChartKpi.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../style/chartKpi.css";
-import { PieChart, Pie, Cell, Label, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 function ChartKpi({ score }) {
 	const data = [{ value: score }, { value: 1 - score }];
@@ -8,15 +8,29 @@ function ChartKpi({ score }) {
 
 	return (
 		<article className="chartKpi">
+			<div className="chartKpi__title">
+					Score
+			</div>
 			<ResponsiveContainer width="100%" height="100%">
 				<PieChart>
-					<circle cx="50%" cy="50%" fill="white" r="60"></circle>
+					<circle cx="50%" cy="50%" fill="white" r="75" className="chartKpi__circle"></circle>
+					  <text x="50%" y="48%" text-anchor="middle">
+          <tspan x="50%" fill="#20253A" className="chartKpi__legend-score">
+            {score * 100}%
+          </tspan>
+          <tspan x="50%" dy="26" fill="#74798C" className="chartKpi__legend-subtext">
+            de votre
+          </tspan>
+          <tspan x="50%" dy="26" fill="#74798C" className="chartKpi__legend-subtext">
+            objectif
+          </tspan>
+        </text>
 					<Pie
 						data={data}
 						cx="50%"
 						cy="50%"
-						innerRadius={60}
-						outerRadius={70}
+						innerRadius={75}
+						outerRadius={85}
 						cornerRadius={10}
 						startAngle={90}
 						endAngle={360 + 90}
@@ -25,7 +39,6 @@ function ChartKpi({ score }) {
 						{data.map((entry, index) => (
 							<Cell key={`cell-${index}`} fill={colors[index % colors.length]} strokeWidth={0} />
 						))}
-						<Label value={score * 100} offset={0} position="center" />
 					</Pie>
 				</PieChart>
 			</ResponsiveContainer>
