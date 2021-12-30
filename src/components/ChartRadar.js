d99587704a030d71bc7c4fbdf4f1c242037b0112
Chloé R <clositi@live.fr>
2021-12-30 23:51:38 +0100

    finished style of ChartRadar

--- src/components/ChartRadar.js
+++ src/components/ChartRadar.js
@@ -3,14 +3,24 @@ import "../style/chartRadar.css";
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from "recharts";
 
 function ChartRadar({ performance }) {
+
 	const data = performance;
+
+	const CustomAxis =(tick) =>{
+		if(tick === 1){return "Cardio"}
+		if(tick === 2){return "Energie"}
+		if(tick === 3){return "Endurance"}
+		if(tick === 4){return "Force"}
+		if(tick === 5){return "Vitesse"}
+		if(tick === 6){return "Intensité"}
+	}
 
 	return (
 		<article className="chartRadar">
 			<ResponsiveContainer width="100%" height="100%">
-				<RadarChart cx="50%" cy="50%" outerRadius={150} data={data}>
-					<PolarGrid radialLines={false} stroke="white" />
-					<PolarAngleAxis dataKey="kind" tick={true} />
+				<RadarChart cx="50%" cy="50%" outerRadius={90} data={data}>
+					<PolarGrid radialLines={false} stroke="white" strokeWidth={1.2}/>
+					<PolarAngleAxis dataKey="kind" tick={true} tickLine={false} tickFormatter={CustomAxis} fontSize={12} stroke="white"/>
 					<Radar name="Performance" dataKey="value" stroke="#FF0000" fill="#FF0000" fillOpacity={0.6} />
 				</RadarChart>
 			</ResponsiveContainer>
